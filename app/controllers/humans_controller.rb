5d5eee679de12d362c57b7527fe6212a4e5f20f1
David Fonvielle <dfonvielle@hotmail.com>
2012-03-16 11:27:51 -0500

    Finish layout and routes

---
+++ app/controllers/humans_controller.rb
@@ -0,0 +1,4 @@
+class HumansController < ApplicationController
+  def new
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,16 @@
 HartlPracticeApp::Application.routes.draw do
   
-  get "static_pages/home"
+  root to: "static_pages#home"
+  
+  get "humans/new"
+  
+  match '/signup', :to => "humans#new"
 
-  get "static_pages/help"
+  match '/help', to: 'static_pages#help'
   
-  get "static_pages/about"
+  match '/about', :to => 'static_pages#about'
   
-  get "static_pages/contact"
+  match '/contact', :to => 'static_pages#contact'
 
   resources :microposts
 
